255d54e71dcf5501120c70730c6aca3df957e9ee
d-how <d-how@DESKTOP-U90V45M>
2023-11-12 10:35:18 +0000

    Initial setup

---
+++ impwrme2/src/test/java/com/impwrme/impwrme2/Impwrme2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impwrme.impwrme2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Impwrme2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
